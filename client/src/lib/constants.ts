c64dda118a0de97084d9516c2d9dad28fe7b5cc6
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-11-09 23:40:58 +0530

    solved lint error

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const SIGN_UP_BACKEND_URL = `${process.env.NEXT_PUBLIC_MONGO_API_URL}/post/user`;
-export const UPDATE_USER_BACKEND_URL = `${process.env.NEXT_PUBLIC_MONGO_API_URL}/api/user/update`;
+export const SIGN_UP_BACKEND_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/post/user`;
+export const UPDATE_USER_BACKEND_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/update`;
