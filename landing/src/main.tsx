299c925dd651c40c216baeacad67fb1448433aef
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-11-09 15:47:43 +0530

    initialized animate presence

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AnimatePresence } from "framer-motion";
+
 import "./index.css";
 import App from "./app.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <AnimatePresence>
+            <App />
+        </AnimatePresence>
     </StrictMode>,
 );
