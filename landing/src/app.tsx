41fe1b7da4364b957f214c77ee5c1b4aa550433b
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-11-09 15:59:14 +0530

    added about section

--- landing/src/app.tsx
+++ landing/src/app.tsx
@@ -1,3 +1,4 @@
+import About from "./components/about";
 import Navbar from "@/components/navbar";
 import TryNow from "./components/try-now";
 
@@ -5,6 +6,7 @@ export default function App() {
     return (
         <main className="flex min-h-screen flex-col bg-custom-white space-y-5 font-helvetica">
             <Navbar />
+            <About />
             <TryNow />
         </main>
     );
