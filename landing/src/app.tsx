07884c3b6b050a7e633df50816822be560b66bd1
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-11-09 16:05:59 +0530

    added images

--- landing/src/app.tsx
+++ landing/src/app.tsx
@@ -1,11 +1,13 @@
-import About from "./components/about";
+import Hero from "@/components/hero";
+import About from "@/components/about";
 import Navbar from "@/components/navbar";
-import TryNow from "./components/try-now";
+import TryNow from "@/components/try-now";
 
 export default function App() {
     return (
         <main className="flex min-h-screen flex-col bg-custom-white space-y-5 font-helvetica">
             <Navbar />
+            <Hero />
             <About />
             <TryNow />
         </main>
